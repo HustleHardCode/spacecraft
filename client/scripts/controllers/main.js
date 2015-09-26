5f1fb74c84514263876fe4eb33acf90b93ec8741
Skurishin Vladislav <vskurishin@marathonbet.ru>
2015-09-26 16:48:10 +0300

    Add npm, bower. Add code structure.

---
+++ client/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spacecraft.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spacecraft
+ */
+angular.module('spacecraft')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
