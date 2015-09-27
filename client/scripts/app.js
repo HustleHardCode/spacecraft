e1e316511503a5bfbe4622920cd18a80f605b925
Skurishin Vladislav <vskurishin@marathonbet.ru>
2015-09-27 19:16:38 +0300

    Add ace-ui framework.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,18 +9,12 @@
  * Main module of the application.
  */
 angular.module('spacecraft', [
-        'ui.router'
-    ])
-    .config(function($stateProvider, $urlRouterProvider)
+    'ui.router',
+    'ui.ace',
+    'spacecraft.main'
+])
+    .config(['$urlRouterProvider', function ($urlRouterProvider)
     {
         // For any unmatched url, send to ""
-        $urlRouterProvider.otherwise("");
-
-        // Our first state called `menu`
-        $stateProvider
-            .state('game', {
-                url: '',
-                templateUrl: '/views/main.html',
-                controller: 'GameController as ctrl'
-            });
-    });
+        $urlRouterProvider.otherwise("/");
+    }]);
--- client/scripts/controllers/main.js
+++ client/scripts/controllers/main.js
@@ -7,9 +7,30 @@
  * # MainCtrl
  * Controller of the spacecraft
  */
-angular.module('spacecraft')
-    .controller('GameController', function ()
+angular.module('spacecraft.main', [])
+    .config(['$stateProvider', function ($stateProvider)
     {
-        var ctrl = this;
-        // Define our controller here
-    });
+        // Our first state called `menu`
+        $stateProvider
+            .state('game', {
+                url: '',
+                templateUrl: '/views/main.html',
+                controller: 'MainController as ctrl'
+            });
+    }])
+    .controller('MainController', ['$scope', function ($scope)
+    {
+        var editorSession,
+            editorRenderer;
+
+        $scope.aceLoaded = function (editor)
+        {
+            editorSession = editor.getSession();
+            editorRenderer = editor.renderer;
+        };
+
+        $scope.aceChanged = function ()
+        {
+            console.log(editorSession.getDocument().getValue());
+        };
+    }]);
