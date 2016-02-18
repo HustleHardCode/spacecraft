b7795d72317e483909aade601d0eda82658177f6
Skurishin Vladislav <vskurishin@marathonbet.ru>
2016-02-18 21:02:18 +0300

    issue#210 some fix

--- public/javascripts/code/lesson1/1.js
+++ public/javascripts/code/lesson1/1.js
@@ -1,22 +1,30 @@
-// Когда я писал этот код, только Бог и я понимали, что он означает. Теперь понимает только Бог.
+// Когда я писал этот код, только Бог и я понимали, что он означает.
+// Теперь понимает только Бог.
 var spaceCraft =
 {
-	fire: function () {
+	fire: function ()
+	{
 		return 'SpaceCraft - fire'
 	},
-	moveForward: function () {
+	moveForward: function ()
+	{
 		return ' SpaceCraft move forward ';
 	},
-	rotateLeft: function () {
+	rotateLeft: function ()
+	{
 		return ' SpaceCraft rotate right ';
 	},
-	rotateRight: function () {
+	rotateRight: function ()
+	{
 		return ' SpaceCraft rotate left ';
 	}
 };
 
 BBotDebug(spaceCraft.moveForward());
-for (var i = 0; i < Math.min(1, 3); ++i)
+
+for (var i = 0; i < 1; ++i)
+{
 	BBotDebug(spaceCraft.fire());
+}
 BBotDebug(spaceCraft.rotateLeft());
 BBotDebug(' BBot ');
